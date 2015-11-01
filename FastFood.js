f9d59b52262a1d635ee9c6150845b7107b050bb9
Hero <hypercraftpe@gmail.com>
2015-11-01 17:14:17 -0500

    Syntax error

--- FastFood.js
+++ FastFood.js
@@ -1,10 +1,11 @@
 ModPE.setFoodItem (483, "hamburger", 0, 1,"Hamburger", 4);
 ModPE.setFoodItem (484, "taco", 0, 1, "Taco", 4);
-Item.addShapedRecipe(483, 4, 0, [
+Item.addShapedRecipe(483,1,0, [
  " x ",
  "azy",
  " x "
- ], ["x", 297, 0, "a", 351, 0, "z", 364, 0, "y", 351, 0])
+ ],
+ ["x",297,0,"a",351,0,"z",364,0,"y",351,0]);
 
 function newLevel() {
  clientMessage(ChatColor.YELLOW+"Fast Food mod created by " ChatColor.BLUE+"@HeroGamingZ");
