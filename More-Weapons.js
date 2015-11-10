88e3e86f8a4095570340226f8dd6a64d098f633c
Hero <hypercraftpe@gmail.com>
2015-11-09 22:50:48 -0500

    Added Commands

--- More-Weapons.js
+++ More-Weapons.js
@@ -14,7 +14,25 @@ Item.addCraftRecipe(842, 1, 0 [352, 1, 0, 325, 1, 0, 266, 1, 0]);
 //Sword Powers
 if (getCarriedItem() == 841) {
     Entity.setHealth(victim, 4);
+    Entity.setSneaking(victim, true);
 }
 if (getCarriedItem() == 842) {
     Entity.setFireTicks(victim, 10);
 }
+//Extra
+function newLevel() {
+    clientMessage(ChatColor.BLUE+"More Weapons by "+ChatColor.GOLD+"@HCMCPE"+ChatColor.BLUE+" Version 1.0");
+}
+function procCmd(command) {
+    var cmd = command.split("");
+    if(cmd[0] == "weapon"){
+        Player.addItemInventory(841, 1, 0);
+    }
+}
+function procCmd(command) {
+    var cmd = command.split("");
+    if(cmd[0] == "heal"){
+        Player.setHealth(20);
+    }
+}
+        
