8a04cdf7d35c1251d08528cbaf5f3180fc44c5f5
Hero <hypercraftpe@gmail.com>
2015-11-10 22:57:42 -0500

    [NEW] Troll Sword
    Makes player explode and changes their name to Noob

--- More-Weapons.js
+++ More-Weapons.js
@@ -3,11 +3,14 @@
 //Inventory
 Player.addItemCreativeInv(841, 1, 0);
 Player.addItemCreativeInv(842, 1, 0);
+Player.addItemCreativeInv(843, 1, 0);
 addItemInventory(841,1);
 addItemInventory(842,1);
+addItemInventory(843,1);
 //Items
 ModPE.setItem(841, "katana", 0, "The Katana", 1);
 ModPE.setItem(842, "lava_sword", 0, "Lava Sword", 1);
+ModPE.setItem(843, "troll_sword", 0, "Troll Sword", 1);
 //Craft Recipes
 Item.addCraftRecipe(841, 1, 0 [352, 1, 0, 49, 1, 0, 265, 1, 0]);
 Item.addCraftRecipe(842, 1, 0 [352, 1, 0, 325, 1, 0, 266, 1, 0]);
@@ -19,6 +22,12 @@ if (getCarriedItem() == 841) {
 if (getCarriedItem() == 842) {
     Entity.setFireTicks(victim, 10);
 }
+if (getCarriedItem() == 843) {
+    Entity.setNameTag(victim, "Noob");
+}
+function attackHook(attacker, victim){
+    Level.explode(Entity.getX(victim), Entity.getY(victim), Entity.getZ(victim), 1);
+}
 //Extra
 function newLevel() {
     clientMessage(ChatColor.BLUE+"More Weapons by "+ChatColor.GOLD+"@HCMCPE"+ChatColor.BLUE+" Version 1.0");
@@ -29,6 +38,7 @@ function procCmd(command) {
         Player.addItemInventory(841, 1, 0);
     }
 }
+//A heal command to gain back your health :)
 function procCmd(command) {
     var cmd = command.split("");
     if(cmd[0] == "heal"){
@@ -36,3 +46,5 @@ function procCmd(command) {
     }
 }
         
+
+
